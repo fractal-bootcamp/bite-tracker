02fa31907b8c7aba916751ca480d58f04b55c3e0
Isaac Gemal <isaacgemal@gmail.com>
2024-12-02 17:56:53 -0500

    ✨ Feat: express backend

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+console.log("Hello via Bun!");
