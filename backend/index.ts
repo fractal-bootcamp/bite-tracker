ddf9ee396c001896778942865f838b72a0feeb31
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-12-03 12:24:51 -0500

    set up clerk webhook handler

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import multer from "multer";
 import cors from "cors";
+import { Webhook } from "svix";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,13 @@ const port = process.env.PORT || 3000;
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(express.json());
+app.use((req, res, next) => {
+  if (req.originalUrl === "/clerk-webhook") {
+    next();
+  } else {
+    express.json()(req, res, next);
+  }
+});
 
 // Add CORS middleware
 app.use(cors());
@@ -33,6 +40,43 @@ app.post(
     res.json({
       message: `File uploaded: ${req.file.originalname}`,
     });
+  }
+);
+
+app.post(
+  "/clerk-webhook",
+  express.raw({ type: "*/*" }),
+  (req: express.Request, res: express.Response): void => {
+    const secret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!secret) {
+      console.error("Missing CLERK_WEBHOOK_SECRET");
+      res.status(500).send("Missing CLERK_WEBHOOK_SECRET");
+      return;
+    }
+    if (
+      typeof req.headers["svix-id"] !== "string" ||
+      typeof req.headers["svix-timestamp"] !== "string" ||
+      typeof req.headers["svix-signature"] !== "string"
+    ) {
+      console.warn("Missing svix headers/wrong types");
+      res.status(400).send("Missing svix headers");
+      return;
+    }
+    const header = {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestamp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    };
+
+    const wh = new Webhook(secret);
+    const payload = wh.verify(req.body, header);
+    if (!payload) {
+      console.warn("Invalid wh payload");
+      res.status(400).send("Invalid payload");
+      return;
+    }
+    res.sendStatus(200);
+    return;
   }
 );
 
